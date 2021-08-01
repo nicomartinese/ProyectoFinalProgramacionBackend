2411ad2ab32c1e50c9fc1b1a672f4451305fba56
nicomartin.s <nicomartinese@gmail.com>
2021-07-31 23:35:20 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+/*
+type funcionSinVariables = () => void;
+
+const fun:funcionSinVariables =() => {
+    console.log('HOLA');
+}
+fun();
+
+type funcionConVariables =(param1:string, param2:number)=>number;
+
+const fun2:funcionConVariables = (param1:string, param2:number)=>{
+    console.log(`${param1} tiene ${param2} años de edad`);
+    return param2;
+}
+console.log(fun2('nico',26));
+
+type funcionNumero = (num1:number, num2:number)=> number
+
+const promedio:funcionNumero = (number1:number, number2:number)=>{
+    const resultado = (number1+number2)/2;
+    return resultado;
+}
+console.log(promedio(2,4));
+
+const prom = (n1:number, n2:number)=>{
+    return ((n1+n2)/2);
+}
+
+console.log(prom(6,12));
+
+
+const nombreCompleto = (nombre:string, apellido:string)=>{
+    return `${nombre} ${apellido}`
+}
+const datos = (nombre:any, edad:number)=>{
+    return `La edad de ${nombre} es ${edad}`
+}
+console.log(datos(nombreCompleto('nico','senra'),26));
+*/
+const suma = (a:number,b:number)=>a+b;
+
+const resta = (a:number,b:number)=>a-b;
+
+const multiplicacion = (a:number,b:number)=>a*b;
+
+const division = (a:number,b:number)=>a/b;
+
+const operacion =(n1:number,n2:number,op:any)=>(op(n1,n2));
+
+console.log(operacion(2,3,suma));
+
+console.log(operacion(2,3,resta));
+
+console.log(operacion(2,3,multiplicacion));
+
+console.log(operacion(2,3,division));
